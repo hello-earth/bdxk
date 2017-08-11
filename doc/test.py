1a2f783ad4a33d554d2b2bc30982b6d42398b3fd
hello-earth <abcd=1234>
2017-08-11 17:21:04 +0800

    init

--- doc/test.py
+++ doc/test.py
@@ -45,7 +45,7 @@ while i<167:
 print
 print u"最大保存个数%d"%int(hexs[167*2:169*2],16)
 print hexs[169*2:]
-'''
+
 
 hexint = []
 hexs="557A100035002820946508000034170806094018015418E6FFFC18E618E6A1E320201FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF42442D444358313502"
@@ -88,3 +88,40 @@ while i<59:
     i+=1
 print
 print hexs[59*2:]
+'''
+
+hexint = []
+hexs="557A0F00310042442D444358313528209465080000341708111625561F014518DD104B00005179104A104D18DD18DD18DDFFFCA1E32020A1"
+for i in xrange(len(hexs)/2):
+    hexint.append(int(hexs[i*2:(i+1)*2], 16))
+
+i=6
+while i<14:
+    if(hexint[i]!=0xFF):
+        print chr(hexint[i]),
+    i+=1
+print
+print u"传感器编号"+hexs[28:44]
+print u"测量日期 "+hexs[22*2:28*2]
+model = bin(hexint[28]).replace("0b","")
+print u"小数点位数:%d"%int(model[:-4], 2)
+print u"传感器类型:%d"%int(model[-4:], 2)
+print u"温度 %.2f ℃"%(int(hexs[29*2:31*2],16)*0.1)
+print u"弦1频率 %.2f Hz"%(int(hexs[31*2:33*2],16)*0.1)
+print u"弦2频率 %.2f Hz"%(int(hexs[33*2:35*2],16)*0.1)
+print u"弦3频率 %.2f Hz"%(int(hexs[35*2:37*2],16)*0.1)
+print u"弦4频率 %.2f Hz"%(int(hexs[37*2:39*2],16)*0.1)
+print u"弦5频率 %.2f Hz"%(int(hexs[39*2:41*2],16)*0.1)
+print u"弦6频率 %.2f Hz"%(int(hexs[41*2:43*2],16)*0.1)
+print u"平均频率 %.2f Hz"%(int(hexs[43*2:45*2],16)*0.1)
+print u"平均频率 %.2f Hz(温度补偿后)"%(int(hexs[45*2:47*2],16)*0.1)
+print u"应变值 %.2f"%(int(hexs[47*2:49*2],16)*0.1)
+print u"偏移值 %.2f"%(int(hexs[49*2:51*2],16)*0.1)
+print u"应变单位",
+i=51
+while i<55:
+    if(hexint[i]!=0xFF):
+        print chr(hexint[i]),
+    i+=1
+print
+print hexs[55*2:]
